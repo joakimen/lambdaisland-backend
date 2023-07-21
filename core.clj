e6f391addc137ea55a6c197e4a8362ec84d650a9
Joakim Lindeng Engeset <joakim.engeset@gmail.com>
2023-07-21 15:02:25 +0200

    Add sample app

---
+++ core.clj
@@ -0,0 +1,43 @@
+(ns lambdaisland-backend.core
+  (:gen-class)
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.defaults :as ring-defaults]
+            [compojure.core :as c]
+            [hiccup2.core :as h]))
+
+(defonce server (atom nil))
+
+(defn home-view []
+  [:html [:body [:h1 "Welcome home!"]]])
+
+(c/defroutes app-routes
+  (c/GET "/" []
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (str (h/html (home-view)))})
+  (c/GET "/:name" [name]
+    {:status 200
+     :body  (str "Hello " name "!")}))
+
+(defn handler [req]
+  (app-routes req))
+
+(defn start-jetty! [& _]
+  (reset! server (jetty/run-jetty
+                  (ring-defaults/wrap-defaults #'handler ring-defaults/site-defaults)
+                  {:join? false
+                   :port 3428})))
+
+(defn stop-jetty! []
+  (.stop @server)
+  (reset! server nil))
+
+(defn -main [& _]
+  (reset! server (start-jetty!)))
+
+(comment
+
+  (start-jetty!)
+  (stop-jetty!)
+  ;;
+  )
